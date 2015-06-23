bb2d5efd3faf6eab0c48c8e9a1fce9d0fdde6982
April Orange <aprilorange.net@icloud.com>
2015-06-23 16:28:29 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var
+  gulp = require('gulp'),
+  uglify = require('gulp-uglify'),
+  jade = require('gulp-jade'),
+  stylus = require('gulp-stylus'),
+  sourcemaps = require('gulp-sourcemaps')
+	
+
+gulp.task('js', function() {
+  gulp.src('./src/js/biu.js')
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./'))
+})
+
+gulp.task('html', function() {
+  gulp.src('./src/jade/index.jade')
+    .pipe(jade())
+    .pipe(gulp.dest('./'))
+})
+
+gulp.task('css', function() {
+  gulp.src('./src/styl/biu.styl')
+    .pipe(stylus({compress: true}))
+    .pipe(gulp.dest('./'))
+})
+
+gulp.task('watch', function() {
+  gulp.watch('./src/jade/index.jade', ['html'])
+  gulp.watch('./src/styl/biu.styl', ['css'])
+  gulp.watch('./src/js/biu.js', ['js'])
+})
+
+gulp.task('build', ['js', 'html'])
+
+gulp.task('default', ['build', 'watch'])
---
+++ src/js/biu.js
@@ -0,0 +1,71 @@
+!(function () {
+
+  var biuOpts = {
+    height: '50px',
+    lineHeight: '50px',
+    top: '-55px',
+    closeButton: 'x',
+    autoFade: true
+  };
+  window.biuOpts = biuOpts;
+  var style = 'height:' + biuOpts.height + ';line-height:' + biuOpts.lineHeight + ';top:' + biuOpts.top;
+
+  var biu = function(argv) {
+    console.log(argv);
+    var div = document.createElement('div');
+    var instance = new Date().getTime();
+    instance = 'biu-instance-' + instance;
+    div.id = instance;
+    var biuStyle = 'info';
+    var biuContent = null;
+    var biuAutoFade = true;
+    if(typeof argv[0] === 'object') {
+
+      var opt = argv[0];
+      if(opt.type) biuStyle = opt.type;
+      if(opt.text) biuContent = opt.text;
+      biuAutoFade = (typeof opt.autoFade !== 'undefined') ? opt.autoFade : true;
+    } else if(argv[0] && !argv[1]) {
+      // biu('text') === biu('info', 'text')
+      biuContent = argv[0];
+    } else {
+
+      biuStyle = argv[0];
+      biuContent = argv[1];
+      biuAutoFade = (typeof argv[2] !== 'undefined') ? argv[2] : true;
+    }
+
+    // set biu style
+    biuStyle = 'biu-' + biuStyle;
+    div.classList.add('biu-instance');
+    div.classList.add(biuStyle);
+    div.setAttribute('style', style);
+    div.setAttribute('data-auto-hide', biuAutoFade);
+    // set text content
+    biuContent = '<div class="biu-content">' + biuContent + '</div>';
+    if(!biuAutoFade) {
+      //var closeButtonTop =  (parseInt(biuOpts.height)/2) + 'px';
+      var biuCloseButton = '<div class="biu-close" onclick="this.parentNode.classList.remove(\'biu-shown\')">' + biuOpts.closeButton + '</div>';
+      biuContent += biuCloseButton;
+    }
+    div.innerHTML = biuContent;
+
+    document.body.appendChild(div);
+    setTimeout(function() {
+      div.classList.add('biu-shown');
+    }, 100)
+    if(biuAutoFade) {
+      setTimeout(function() {
+        div.classList.remove('biu-shown');
+      }, 5000);
+    }
+    
+  };
+
+  console.log('biu');
+
+  window.biu = function() {
+    return new biu(arguments);
+  };
+
+})();
