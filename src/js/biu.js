7911edfac1a1504ceb44c59ddccc94ea51e759e1
April Orange <aprilorange.net@icloud.com>
2015-06-28 19:59:39 +0800

    Biu in elements other than body :100:

--- src/js/biu.js
+++ src/js/biu.js
@@ -2,6 +2,32 @@
   // my code is shitty
   // feel free to add your own features
   // or beautify it
+
+  function queryDom(p) {
+    var z = 'getElement',
+        g = 'Name',
+        a = {'#': 'ById', '.': 'sByClass' + g, '@': 'sBy' + g, '=': 'sByTag' + g}[p[0]] || false,
+        b = p.slice(1),
+        u = (a ? document[z + a](b) : document.querySelectorAll(p));
+
+    var el = (checkType(u, 'NodeList')) ? Array.prototype.slice.call(u) : u;
+    return el;
+  };
+
+  function checkType(obj, type) {
+    return Object.prototype.toString.call(obj) === '[object ' + type + ']';
+  };
+
+  function domEach(element, fn) {
+    if (checkType(element, 'Array')) {
+      Array.prototype.forEach.call(element, function(el, i){
+        fn(el);
+      });
+    } else {
+      fn(element);
+    }
+  };
+
   var biuOpts = {
     height: '50px',
     lineHeight: '50px',
@@ -9,7 +35,7 @@
     closeButton: 'x'
   };
   window.biuOpts = biuOpts;
-  var style = 'height:' + biuOpts.height + ';line-height:' + biuOpts.lineHeight + ';top:' + biuOpts.top;
+  var style = 'height:' + biuOpts.height + ';line-height:' + biuOpts.lineHeight + ';top:' + biuOpts.top + ';';
 
   var biu = function(argv) {
 
@@ -23,13 +49,21 @@
     var biuDelay = 5000;
     var biuStack = false;
     var biuCloseButton = '';
+    var biuParent = queryDom('body');
+    var biuParentIsBody = true;
     if(typeof argv[0] === 'object') {
       var opt = argv[0];
       if(opt.type) biuStyle = opt.type;
       if(opt.text.toString()) biuContent = opt.text;
       biuAutoFade = (typeof opt.autoFade !== 'undefined') ? opt.autoFade : true;
       biuDelay = (typeof opt.delay !== 'undefined') ? opt.delay : 5000;
-      biuStack = (typeof opt.stack !== 'undefined') ? opt.stack : false;
+      if(typeof opt.parent !== 'undefined') {
+        biuParent = queryDom(opt.parent);
+        style += 'position:absolute;';
+        if(opt.parent != 'body') {
+          biuParentIsBody = false;
+        }
+      }
     } else if(argv[0] && !argv[1]) {
       // biu('text') === biu('info', 'text')
       biuContent = argv[0];
@@ -54,7 +88,10 @@
     biuContent = '<div class="biu-content">' + biuContent + '</div>' + biuCloseButton;
     div.innerHTML = biuContent;
 
-    document.body.appendChild(div);
+    domEach(biuParent, function(el) {
+      el.appendChild(div);
+    })
+
     setTimeout(function() {
       div.classList.add('biu-shown');
     }, 100)
@@ -70,7 +107,7 @@
       div.classList.remove('biu-shown');
       setTimeout(function() {
         div.parentNode.removeChild(div);
-      }, 1000);
+      }, 600);
     };
   };
 
